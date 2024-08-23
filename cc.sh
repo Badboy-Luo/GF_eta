12be5a6fb94eae4cd4060effa1d214c6558e2fe5
Bingxu Luo <896359862@qq.com>
2024-08-23 11:11:32 -0700

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -6,7 +6,7 @@ msnoise db init --tech 1
 
 msnoise config set startdate=2019-08-01
 msnoise config set enddate=2021-08-31
-msnoise config set components_to_compute=ZZ
+msnoise config set components_to_compute=ZZ   # NN, EE or ZZ depending on which component you use
 # msnoise config set remove_response='Y'
 # msnoise config set response_format=inventory
 # msnoise config set response_path='./station'
@@ -29,7 +29,7 @@ msnoise config set maxlag=3
 msnoise config set overlap=0.5
 msnoise config set keep_days=Y
 msnoise config set analysis_duration=86400  # 1 day
-msnoise config set corr_duration=1800  # 6h
+msnoise config set corr_duration=1800  # seconds
 msnoise config set hpc='Y'
 
 msnoise db execute 'insert into filters (ref, low, mwcs_low, high, mwcs_high, rms_threshold, mwcs_wlen, mwcs_step, used) values (1, 1, 1, 99, 99, 0.0, 12.0, 4.0, 1)'
